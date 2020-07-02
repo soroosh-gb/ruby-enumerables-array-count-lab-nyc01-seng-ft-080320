8f13bbb8a32cb950a635eca8b1cc7a3d761b3791
Soroosh Golbabae <soroosh.gb@gmail.com>
2020-07-02 22:23:43 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -6,6 +6,3 @@ def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
 
-[0, 1, -9, 24, 5, -10].count do
-  |x| > 0 
-end 
